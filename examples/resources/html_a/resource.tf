b5582d7b64089035f03eaf421d7e1209058932fa
koki-develop <kou.pg.0131@gmail.com>
2024-07-21 12:02:37 +0900

    Add example for a

--- examples/resources/html_a/resource.tf
+++ examples/resources/html_a/resource.tf
@@ -3,3 +3,15 @@ resource "html_a" "simple" {
   href     = "https://example.com"
 }
 # => <a href="https://example.com">Click me!</a>
+
+resource "html_a" "download" {
+  children = ["Download"]
+  download = true
+  href     = "https://example.com/file"
+}
+# => <a download href="https://example.com/file">Download</a>
+
+resource "html_a" "nolink" {
+  children = ["No link"]
+}
+# => <a>No link</a>
--- examples/resources/html_a/resource.tftest.hcl
+++ examples/resources/html_a/resource.tftest.hcl
@@ -4,3 +4,17 @@ run "simple" {
     error_message = ""
   }
 }
+
+run "download" {
+  assert {
+    condition     = html_a.download.html == "<a download href=\"https://example.com/file\">Download</a>"
+    error_message = ""
+  }
+}
+
+run "nolink" {
+  assert {
+    condition     = html_a.nolink.html == "<a>No link</a>"
+    error_message = ""
+  }
+}
