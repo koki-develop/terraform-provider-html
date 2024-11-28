03697df3e27b1b19b0bab9a39eb94b977ca04c0e
koki-develop <kou.pg.0131@gmail.com>
2024-11-28 23:03:13 +0900

    Implement util.MinifyHTML

--- internal/util/util.go
+++ internal/util/util.go
@@ -3,10 +3,37 @@ package util
 import (
 	"context"
 	"fmt"
+	"regexp"
 
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/tdewolff/minify/v2"
+	"github.com/tdewolff/minify/v2/css"
+	"github.com/tdewolff/minify/v2/html"
+	"github.com/tdewolff/minify/v2/js"
+	"github.com/tdewolff/minify/v2/json"
+	"github.com/tdewolff/minify/v2/svg"
+	"github.com/tdewolff/minify/v2/xml"
 )
+
+var (
+	patternJS   = regexp.MustCompile("^(application|text)/(x-)?(java|ecma)script$")
+	patternJSON = regexp.MustCompile("[/+]json$")
+	patternXML  = regexp.MustCompile("[/+]xml$")
+)
+
+func MinifyHTML(s string) (string, error) {
+	m := minify.New()
+
+	m.AddFunc("text/css", css.Minify)
+	m.AddFunc("text/html", html.Minify)
+	m.AddFunc("image/svg+xml", svg.Minify)
+	m.AddFuncRegexp(patternJS, js.Minify)
+	m.AddFuncRegexp(patternJSON, json.Minify)
+	m.AddFuncRegexp(patternXML, xml.Minify)
+
+	return m.String("text/html", s)
+}
 
 func StringifyAttribute(ctx context.Context, name string, value types.Dynamic) (string, error) {
 	v := value.UnderlyingValue()
